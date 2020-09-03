5b9064a8d78e01ad2737e5941607cd9fa4a3d145
Ozgur Dincer <ozgurdincer@kentajans.com>
2020-09-03 10:15:34 +0300

    ready to upload

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let siteTitle;
+</script>
+<div id="wrapper">
+	<div class="main">
+		<div class="header">
+			<div class="logo">
+				<h1 class="logo">{siteTitle}</h1>
+				<h2>the oldtimer .. the youngtimer</h2>
+			</div>
+		</div>
+		<div class="launch_main">
+			<div class="launch">
+				<ul class="social_one">
+					<li class="facebook"><a href="https://www.facebook.com/MercedesBenzClassicGarage?ref=website"></a></li>
+					<li class="twitter"><a href="https://twitter.com/MercedesBenzCCG"></a></li>
+				</ul>
+				<p class="launching">Launching Soon!</p>
+				<ul class="social_two">
+					<li class="youtube"><a href="https://www.facebook.com/groups/mercedesbenzclassicgarage/"></a></li>
+					<li class="instagram"><a href="https://www.instagram.com/raymotors_classicgarage/"></a></li>
+				</ul>
+			</div>
+		</div>
+		
+
+		<div class="content">
+			<p>Discover the taste of renovation oldtimer Mercedes-Benz Classics soon at the garage near you.</p>
+			<p class="info"><a href="mailto:mercedesbenzclassicgarage@gmail.com">mercedesbenzclassicgarage@gmail.com</a><span>/</span>-<span>/</span>Istanbul</p>
+		</div>
+	</div>
+
+</div>
