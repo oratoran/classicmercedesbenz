854f69e9b0e13a8cc078258c9b8a0aa7b36c58c3
Ozgur Dincer <ozgurdincer@kentajans.com>
2020-09-03 12:26:04 +0300

    minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -12,13 +12,13 @@
 		<div class="launch_main">
 			<div class="launch">
 				<ul class="social_one">
-					<li class="facebook"><a href="https://www.facebook.com/MercedesBenzClassicGarage?ref=website"></a></li>
-					<li class="twitter"><a href="https://twitter.com/MercedesBenzCCG"></a></li>
+					<li class="facebook"><a href="https://www.facebook.com/MercedesBenzClassicGarage?ref=website" aria-label="Facebok Page"> </a></li>
+					<li class="twitter"><a href="https://twitter.com/MercedesBenzCCG" aria-label="Twitter Page"> </a></li>
 				</ul>
 				<p class="launching">Launching Soon!</p>
 				<ul class="social_two">
-					<li class="youtube"><a href="https://www.facebook.com/groups/mercedesbenzclassicgarage/"></a></li>
-					<li class="instagram"><a href="https://www.instagram.com/raymotors_classicgarage/"></a></li>
+					<li class="youtube"><a href="https://www.facebook.com/groups/mercedesbenzclassicgarage/" aria-label="Faecbook Group"> </a></li>
+					<li class="instagram"><a href="https://www.instagram.com/raymotors_classicgarage/" aria-label="Instagram Page"> </a></li>
 				</ul>
 			</div>
 		</div>
@@ -26,7 +26,7 @@
 
 		<div class="content">
 			<p>Discover the taste of renovation oldtimer Mercedes-Benz Classics soon at the garage near you.</p>
-			<p class="info"><a href="mailto:mercedesbenzclassicgarage@gmail.com">mercedesbenzclassicgarage@gmail.com</a><span>/</span>-<span>/</span>Istanbul</p>
+			<p class="info"><a href="mailto:mercedesbenzclassicgarage@gmail.com" aria-label="Email adress">mercedesbenzclassicgarage@gmail.com</a><span>/</span>-<span>/</span>Istanbul</p>
 		</div>
 	</div>
 
